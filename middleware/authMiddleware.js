739acbeb0923becc52f88d2f41aa9c8d4c0c075e
khanhduy1407 <kn145660@gmail.com>
2022-01-13 21:52:06 +0700

    Checking the Current User

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -7,7 +8,7 @@ const requireAuth = (req, res, next) => {
     if (token) {
         jwt.verify(token, "nkduy secret", (err, decodedToken) => {
             if (err) {
-                console.log(err.message)
+                console.log(err.message);
                 res.redirect('/login');
             } else {
                 console.log(decodedToken);
@@ -18,4 +19,28 @@ const requireAuth = (req, res, next) => {
         res.redirect('/login');
     }
 }
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "nkduy secret", async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
